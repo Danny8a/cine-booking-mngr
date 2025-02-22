7f363dd7d25f4645a51d08701f5f337ab909dd88
dochoa5 <99045804+dochoa5@users.noreply.github.com>
2025-02-22 16:41:01 -0500

    Initialize booking-cine-mngr application with Maven setup and basic configuration

---
+++ src/main/java/com/booking_cine_mngr/BookingCineMngrApplication.java
@@ -0,0 +1,13 @@
+package com.booking_cine_mngr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingCineMngrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookingCineMngrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/booking_cine_mngr/BookingCineMngrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.booking_cine_mngr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingCineMngrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
